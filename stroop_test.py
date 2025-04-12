89d2e8b1100dcc5bea94e2383ec6789811c71fb8
ckasa2 <130487584+ckasa2@users.noreply.github.com>
2025-04-11 20:33:35 -0500

    outline for stroop test script

---
+++ stroop_test.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+import random
+import time
+import csv
+
+colors = ["red", "blue", "green", "yellow"]
+words = ["red", "blue", "green", "yellow"]
+#outline of the stroop test script
+
+def start_experiment():
+    pass
+
+# function to show stimulus
+def show_stimulus(word, color):
+    pass
+
+# function to record trial result
+def log_data(word, color, response, correct, reaction_time):
+    pass
+
+# function to save data to CSV
+def save_to_csv(data_list):
+    pass
+
+# will call the  main function 
+if __name__ == "__main__":
+    start_experiment()
